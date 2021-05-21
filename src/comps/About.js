5b0394419e553bf6b5d45b77448e602c36bdffab
AbdulazizProgrammer <Abdulazizprogrammer@gmail.com>
2021-05-21 15:34:47 +0500

    about is changed

--- src/comps/About.js
+++ src/comps/About.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return <div id="about">About</div>;
-}
+  return (
+    <div id="about">
+      About Us Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Consequuntur nihil, autem culpa magnam consequatur iusto, quas odit quos
+      neque inventore corrupti dolor itaque nulla veniam. Quisquam sapiente
+      facere voluptatum consequuntur.
+    </div>
+  );
+};
 
-export default About
+export default About;
